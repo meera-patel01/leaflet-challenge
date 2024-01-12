9488fad17ea90e779210a05ba9f6c9ad4599dabe
meera-patel01 <meera.patel2k19@gmail.com>
2024-01-11 20:25:04 -0600

    final

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -2,50 +2,22 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
   createMarkers(data.features);
 });
 
-function markerSize(magnitude) {
-  return magnitude * 15000;
-};
-
-function chooseColor(depth) {
+function color(depth) {
       switch(true) {
         case depth > 90:
-          return "#8B0000";
+          return "red";
         case depth > 70:
-          return "#CC5500";
+          return "#FF9248";
         case depth > 50:
-          return "orange";
+          return "#FFB38A";
         case depth > 30:
-          return "yellow";
+          return "gold";
         case depth > 10:
-          return "yellowgreen";
+          return "#D1E231";
         default:
           return "green";
       }
     }
-
-
-
-function createMarkers(earthquake) {
-
-  function onEachFeature(feature, layer) {
-    layer.bindPopup("<h3>Location: " + feature.properties.place + "<h3><h3>Magnitude: " + feature.properties.mag + "</h3>");
-  }
-
-  var earthquakes = L.geoJSON(earthquake, {
-    onEachFeature: onEachFeature,
-    pointToLayer: function(feature, latlng) {
-      let markers = {
-        radius: markerSize(feature.properties.mag),
-        fillColor: chooseColor(feature.geometry.coordinates[2]),
-        color: chooseColor(feature.geometry.coordinates[2]),
-        stroke: true,
-      }
-      return L.circle(latlng,markers);
-    }
-  });
-
-  createMap(earthquakes);
-}
 
 function createMap(earthquakes) {
 
@@ -67,20 +39,41 @@ function createMap(earthquakes) {
   });
 
 
-let legend = L.control({position: "bottomright"});
-legend.onAdd = function(map) {
-  let div = L.DomUtil.create("div", "info legend"),
-  labels = ['<strong>Depth</strong>'],
-  depth = [-10, 10, 30, 50, 70, 90];
+  let legend = L.control({position: "bottomright"});
+  legend.onAdd = function(map) {
+    let div = L.DomUtil.create("div", "info legend"),
+    labels = ['<strong>Depth</strong>'],
+    depth = [-10, 10, 30, 50, 70, 90];
 
-  for (let i = 0; i < depth.length; i++) {
-    div.innerHTML +=
-    labels.push(
-      '<i class="legend" style="background:' + chooseColor(depth[i] + 1) + '"></i> ' + depth[i] + (depth[i + 1] ? '&ndash;' + depth[i + 1] + '<br>' : '+'));
+    for (let i = 0; i < depth.length; i++) {
+      div.innerHTML +=
+      labels.push(
+        '<i class="legend" style="background:' + color(depth[i] + 1) + '"></i> ' + depth[i] + (depth[i + 1] ? '&ndash;' + depth[i + 1] + '<br>' : '+'));
+    } 
+    div.innerHTML = labels.join('<br>');
+    return div;
+  };
+  legend.addTo(map)
+
+}
+
+function createMarkers(earthquake) {
+
+  function onEachFeature(feature, layer) {
+    layer.bindPopup("<h3>Location: " + feature.properties.place + "<h3><h3>Magnitude: " + feature.properties.mag + "<h3><h3>Date: " + Date(feature.properties.time) + "</h3>");
   }
-  div.innerHTML = labels.join('<br>');
-  return div;
-};
-legend.addTo(map)
 
-} 
+  var earthquakes = L.geoJSON(earthquake, {
+    onEachFeature: onEachFeature,
+    pointToLayer: function(feature, latlng) {
+      let markers = {
+        radius: feature.properties.mag * 15000,
+        fillColor: color(feature.geometry.coordinates[2]),
+        color: color(feature.geometry.coordinates[2])
+      }
+      return L.circle(latlng,markers);
+    }
+  });
+
+  createMap(earthquakes);
+}
